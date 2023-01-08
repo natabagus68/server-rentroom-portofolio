b370bec6f27a2a656c0e5264718ce9f00173cfa3
nata bagus <natabagusarisandi68@gmail.com>
2023-01-08 07:30:43 +0700

    seeding and add column in migration

--- seeders/20230107193232-userSeeder.js
+++ seeders/20230107193232-userSeeder.js
@@ -13,9 +13,37 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
      */
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          email: "natabagus78@gmail.com",
+          password: encryptPassword("test123"),
+          role: "admin",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          email: "natabagusarisandi68@gmail.com",
+          password: encryptPassword("test123"),
+          role: "admin",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          email: "desifitria@gmail.com",
+          password: encryptPassword("test123"),
+          role: "user",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null, {});
     /**
      * Add commands to revert seed here.
      *
